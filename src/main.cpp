0b00b6a61de9411678ad1fa019f90431df0d40fe
joseluisvaz <joseluisv1201@gmail.com>
2018-04-25 19:02:53 -0500

    Refactor of main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-
+#include <stdlib.h>
 #include "String.h"
+#include "std_msgs/Int32.h"
 #include "S32K148.h"
 #include "ros.h"
+
 // Include C headers (ie, non C++ headers) in this block
 extern "C" {
 	#include "clocks_and_modes.h"
@@ -11,6 +13,55 @@ extern "C" {
 extern "C" void __cxa_pure_virtual(void);
 void __cxa_pure_virtual(void) {}
 
+// Function prototypes
+void message_callback(const std_msgs::Int32 &msg);
+
+// Provisional test function prototypes
+void init_led(void);
+void led_on(void);
+void led_toggle(void);
+
+ros::Publisher pub("", 0);
+// Subscribers are declared in the main scope due to a compiler bug
+
+int main() {
+
+  SOSC_init_8MHz();       /* Initialize system oscilator for 8 MHz xtal */
+  SPLL_init_160MHz();     /* Initialize SPLL to 160 MHz with 8 MHz SOSC */
+  NormalRUNmode_80MHz();  /* Init clocks: 80 MHz sysclk & core, 40 MHz bus, 20 MHz flash */
+  init_led();
+
+  ros::NodeHandle nh;
+  ros::Subscriber<std_msgs::Int32> sub("/board_connection/test", &message_callback);
+
+  std_msgs::String str_msg;
+  pub.topic_ = "/mcu/chatter";
+  pub.msg_ = &str_msg;
+
+  nh.initNode();
+  nh.advertise(pub);
+  nh.subscribe(sub);
+
+  uint32_t lasttime = 0UL;
+
+  while(1) {
+    if(s32k148_time_now() - lasttime > 10) {
+      // For testing purposes
+      lasttime = s32k148_time_now();
+    }
+    nh.spinOnce();
+  }
+  return 0;
+}
+
+void message_callback(const std_msgs::Int32 &msg) {
+	std_msgs::String str_msg;
+	if(msg.data > 30.0) {
+		str_msg.data = "h";
+		led_toggle();
+		pub.publish(&str_msg);
+	}
+}
 
 void init_led(void){
 	PCC->PCCn[PCC_PORTE_INDEX] = PCC_PCCn_CGC(1);
@@ -22,37 +73,6 @@ void led_on(void) {
 	PTE->PSOR |= 1<<22;
 }
 
-void led_off(void) {
+void led_toggle(void) {
     PTE->PTOR |= 1<<22;
-}
-
-
-int main() {
-
-  ros::NodeHandle nh;
-  std_msgs::String str_msg;
-  ros::Publisher chatter("chatter", &str_msg);
-  char hello[2] = "h";
-
-  SOSC_init_8MHz();       /* Initialize system oscilator for 8 MHz xtal */
-  SPLL_init_160MHz();     /* Initialize SPLL to 160 MHz with 8 MHz SOSC */
-  NormalRUNmode_80MHz();  /* Init clocks: 80 MHz sysclk & core, 40 MHz bus, 20 MHz flash */
-
-  init_led();
-
-  uint32_t lasttime = 0UL;
-
-  // Initialize ROS
-  nh.initNode();
-  nh.advertise(chatter);
-
-  while(1) {
-    if(s32k148_time_now() - lasttime > 10) {
-      str_msg.data = hello;
-      lasttime = s32k148_time_now();
-      chatter.publish(&str_msg);
-    }
-    nh.spinOnce();
-  }
-  return 0;
 }
